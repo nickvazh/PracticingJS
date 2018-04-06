559c8cbec5720272c49419418bde798eeafc1bc4
nickvazh <30155604+nickvazh@users.noreply.github.com>
2018-04-06 16:41:53 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,50 +1,116 @@
-
-
-
-
 function Question (quest, ans1, ans2) {
 
 	this.quest = quest;
 	this.ans1 = ans1;
 	this.ans2 = ans2;
 
+
 }
 
 
-var questionOne = new Question('How are you?', 'good', 'feeling great');
+var questionOne = new Question('Is it Day or Night?', 'yes', 'no');
 var questionTwo = new Question('Do you like candy?', 'yes', 'no');
 var questionThree = new Question('Is it cold today?', 'yes', 'no');
 
-logic();
-Question();
-
-var question1 = prompt(questionOne.quest);
 
 
-function randomQuestion() {
+var questions = [
 
-	randomQ = Math.floor(Math.random() * (questions.length));
-	console.log(questions[randomQ]);
-}
+questionOne.quest,
+questionTwo.quest,
+questionThree.quest
+
+];
+
+var answers = [
+
+questionOne.ans1,
+questionOne.ans2,
+questionTwo.ans1,
+questionTwo.ans2,
+questionThree.ans1,
+questionThree.ans2,
+
+];
+
+var userInput;
+var point = 0;
+var line = '----------------------------------------------';
+
+console.log('Please answer the questions with Yes or No');
+console.log(line);
 
 randomQuestion();
+
+function randomQuestion() {
+    
+	var randomQ = Math.floor(Math.random() * (questions.length));
+	console.log(questions[randomQ]);
+	console.log(line);
+	userInput = prompt(questions[randomQ]);
+	
+
+}
 
 
 function logic() {
 
-	if(questions === questionOne.ans1){
+var rightAnswer = 'Right answer ' + '|| Your score is: ';
+var wrongAnswer = 'Wrong answer ' + '|| Your score is: ';
 
-		console.log('this is the right answer');
 
-	}else if (questionOne.quest === questionOne.ans2){
+	if(userInput === answers[0]){
 
-		console.log('this is the wrong answer');
+		point++;
+		console.log(rightAnswer + point + ' points');
+		console.log(line);
+
+	}else if (userInput === answers[1]){
+
+		console.log(wrongAnswer + point + ' points');
+		console.log(line);
+
+	}else if (userInput === answers[2]){
+
+		point++;
+		console.log(rightAnswer + point + ' points');
+		console.log(line);
+
+	}else if (userInput === answers[3]){
+
+		console.log(wrongAnswer + point + ' points');
+		console.log(line);
+
+	}else if (userInput === answers[4]){
+
+		point++;
+		console.log(rightAnswer + point + ' points');
+		console.log(line);
+
+	}else if (userInput === answers[5]){
+
+		console.log(wrongAnswer + point + ' points');
+		console.log(line);	
 
 	}else {
 
-		console.log('no answer');
+		console.log('no answer ' + '|| Your score is: ' + point + ' points');
+		console.log(line);
 	}
+
 }
 
+logic();
 
+for (var i = 0; i < 4; i++) {
+ 
+ 	randomQuestion();
+ 	
+ 	logic();
 
+}
+
+console.log(line);
+console.log('    --- Your total score is: ' + point + ' points ---');
+console.log(line);
+console.log(line);
